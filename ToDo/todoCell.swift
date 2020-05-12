ce41b867b6da8c6f76f0fee9287163db7c7d1578
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2020-05-13 01:34:34 +0530

    modified cell

--- ToDo/todoCell.swift
+++ ToDo/todoCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class todoCell: UITableViewCell {
 
+    @IBOutlet weak var countLbl: UILabel!
+    @IBOutlet weak var taskLbl: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
